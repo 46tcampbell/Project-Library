6b3df333a57dc13d1a58c0a2142e695c7585753b
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2025-10-22 17:03:13 -0500

    Create initial Library array and logic to add books
    Just followed the steps on the project and created a simple book
    constructor function and an additional function to actually add books
    to the array.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+const myLibrary = [];
+
+function Book(title, author, pages, read, id) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = id;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const id = crypto.randomUUID();
+  const newBook = new Book(title, author, pages, read, id);
+  return myLibrary.push(newBook);
+}
