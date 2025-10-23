e0f6add11e917afc117c37dd60f812fc280ef47b
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2025-10-23 10:50:20 -0500

    Add logic to loop through book array and display
    Used a loop along with DOM manipulation code as I did not know an easier
    way to add a new row to the table. I also added CSS reset and table
    styles on this commit as I forgot to add them earlier.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const table = document.querySelector('#book-table');
 
 function Book(title, author, pages, read, id) {
   this.title = title;
@@ -16,3 +17,44 @@ function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read, id);
   return myLibrary.push(newBook);
 }
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+addBookToLibrary('Test', 'J.R.R. Tolkien', 295, 'not read yet');
+
+// Note from 10/22/25: Started the below function and did not have time to finish. Step 3 from Project
+
+function addBookToTable() {
+  for (const book of myLibrary) {
+    const newRow = document.createElement('tr');
+    const title = document.createElement('td');
+    title.textContent = book.title;
+    newRow.appendChild(title);
+    const author = document.createElement('td');
+    author.textContent = book.author;
+    newRow.appendChild(author);
+    const pages = document.createElement('td');
+    pages.textContent = book.pages;
+    newRow.appendChild(pages);
+    const read = document.createElement('td');
+    read.textContent = book.read;
+    newRow.appendChild(read);
+    table.appendChild(newRow);
+  }
+}
+
+addBookToTable();
+
+/* <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Number of Pages</th>
+          <th>Read Status</th>
+        </tr>
+      </thead>
+      <tbody id="book-table"></tbody>
+    </table> */
